c226a08474fe21ced46058f6fdfe83e92199bcd9
Nata Kmitiuk <lovelynata9@gmail.com>
2022-11-21 19:53:09 +0200

    EightImpl

--- src/main/java/com/org/ita/kata/implementation/KmytiukNatalyia/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/KmytiukNatalyia/EightImpl.java
@@ -5,46 +5,74 @@ import main.java.com.org.ita.kata.Eight;
 public class EightImpl implements Eight {
     @Override
     public int liters(double time) {
-        return 0;
+        return (int)(time/2);
     }
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
+        return (int)(length*width*height);
     }
 
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        return Math.round(mpg * 35.4006044) / 100f;
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        for (int i=0;i < array.length; i++) {
+            double square = Math.sqrt(array[i]);
+            if (square==Math.round(square)) {
+                array[i] = (int)square;
+            } else {
+                array[i] = array[i]*array[i];
+            }
+        }
+        return array;
     }
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if (input == null || input.length == 0) return new int[] {};
+        int count = 0,sum = 0;
+        for (int i : input) {
+            if (i > 0) count ++;
+            if (i < 0) sum = sum + i;
+        }
+        return new int[] {count,sum};
     }
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.parseInt(str);
     }
 
     @Override
     public boolean amIWilson(double n) {
-        return false;
+        return n == 5 || n == 13 || n == 563;
     }
 
     @Override
     public double twoDecimalPlaces(double number) {
-        return 0;
+        return Math.round(number*100.0)/100.0;
     }
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        int count = 0;
+        for (int number : numbers){
+            if (number % divider == 0){
+                count ++;
+            }
+        }
+        int[] arr = new int[count];
+        int i = 0;
+        for (int number : numbers){
+            if (number % divider == 0){
+                arr[i] = number;
+                i++;
+            }
+        }
+        return arr;
     }
 }
