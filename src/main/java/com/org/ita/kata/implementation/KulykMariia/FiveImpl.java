e15508df30ef4ae0c6145e6ed25edfde22d10bf8
merykul <mariakulv@gmail.com>
2022-11-22 15:04:25 +0300

    added some tasks in five impl

--- src/main/java/com/org/ita/kata/implementation/KulykMariia/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/KulykMariia/FiveImpl.java
@@ -12,22 +12,62 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+
+        // g -> (integer >= 2) which indicates the gap we are looking for;
+        // m -> (integer > 2) which gives the start of the search (m inclusive);
+        // n -> (integer >= m) which gives the end of the search (n inclusive);
+
+        long[] pair = new long[2];
+        int k = 0;
+        for (long i = m; i < n; i++) {
+            k++;
+            if (isPrime(i)) {
+                if ((k == g) && (i - g >= m)) {
+                    pair[0] = i - g;
+                    pair[1] = i;
+                    return pair;
+                } else k = 0;
+            }
+        }
+        return null;
+    }
+
+    public static boolean isPrime(long n) {
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0) return false;
+        return true;
     }
 
     @Override
     public int zeros(int n) {
-        return 0;
+        int s = 0;
+        while (n > 4) {
+            s += n / 5;
+            n /= 5;
+        }
+        return s;
     }
 
     @Override
     public BigInteger perimeter(BigInteger n) {
-        return null;
+        BigInteger a;
+        BigInteger b = BigInteger.ONE;
+        BigInteger count = BigInteger.ONE;
+        BigInteger sum = BigInteger.ZERO;
+
+        for(int i = 0; i <= n.intValue(); i++) {
+            a = b;
+            b = count;
+            count = a.add(b);
+            sum = sum.add(a);
+        }
+        return sum.multiply(BigInteger.valueOf(4));
     }
 
     @Override
     public double solveSum(double m) {
-        return 0;
+        double b = -2 * m - 1;
+        return (-b - Math.sqrt(Math.pow(b, 2) - 4 * m * m)) / (2 * m);
     }
 
     @Override
@@ -35,3 +75,4 @@ public class FiveImpl implements Five {
         return new long[0];
     }
 }
+
