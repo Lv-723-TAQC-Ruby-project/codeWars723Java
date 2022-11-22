5376774b258d63b849a7d1c7d3ef2187583e947a
bohdan-khudio <bahdan510@gmail.com>
2022-11-22 21:17:59 +0200

    add SevenImpl tasks

--- src/main/java/com/org/ita/kata/implementation/KhudoBohdan/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/KhudoBohdan/SevenImpl.java
@@ -2,19 +2,37 @@ package main.java.com.org.ita.kata.implementation.KhudoBohdan;
 
 import main.java.com.org.ita.kata.Seven;
 
+import java.util.Arrays;
+
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double result = navg * (arr.length + 1) - Arrays.stream(arr).sum();
+        if (result <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return (long)Math.ceil(result);
     }
 
     @Override
     public String seriesSum(int n) {
-        return null;
+        double x = 1;
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += 1 / x;
+            x += 3;
+        }
+
+        return String.format("%.2f", sum);
     }
 
     @Override
     public int whereIsHe(int p, int bef, int aft) {
-        return 0;
+        int vasya = bef + 1;
+        while (p - vasya > aft) {
+            vasya++;
+        }
+        return p - vasya + 1;
     }
 }
