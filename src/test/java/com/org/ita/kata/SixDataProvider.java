c5f4b693a3d288fc8ef9c244c3fd50697136ff9c
merykul <mariakulv@gmail.com>
2022-11-26 09:16:01 +0300

    rainfall test (not completed)

--- src/test/java/com/org/ita/kata/SixDataProvider.java
+++ src/test/java/com/org/ita/kata/SixDataProvider.java
@@ -3,4 +3,20 @@ package com.org.ita.kata;
 import org.testng.annotations.DataProvider;
 
 public class SixDataProvider extends BaseDataProvider{
+    @DataProvider(name = "Rainfall -> mean")
+    public Object[][] meanRainfallData() {
+        Object[][] testData = new Object[][]{
+                {"London", 51.199999999999996},
+                {"Beijing", 52.416666666666664}
+        };
+        return combineImplWithTests(SIX_IMPL, testData);
+    }
+    @DataProvider(name = "Rainfall -> variance")
+    public Object[][] varianceRainfallData() {
+        Object[][] testData = new Object[][]{
+                {"London", 57.42833333333374},
+                {"Beijing", 4808.37138888889}
+        };
+        return combineImplWithTests(SIX_IMPL, testData);
+    }
 }
