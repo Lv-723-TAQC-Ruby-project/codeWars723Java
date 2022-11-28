23fa6d093e631908785fe98e7faacf831b64cad6
bohdan-khudio <bahdan510@gmail.com>
2022-11-29 00:23:52 +0200

    add FiveImpl tasks

--- src/main/java/com/org/ita/kata/implementation/KhudoBohdan/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/KhudoBohdan/FiveImpl.java
@@ -4,6 +4,7 @@ import com.org.ita.kata.BaseKata;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class FiveImpl extends BaseKata implements Five {
     @Override
@@ -71,21 +72,80 @@ public class FiveImpl extends BaseKata implements Five {
 
     @Override
     public int zeros(int n) {
-        return 0;
+        int count = 0;
+        for (int i = 5; n / i >= 1; i *= 5) {
+            count += n / i;
+        }
+        return count;
+    }
+
+    private static BigInteger fib(BigInteger n) {
+        BigInteger a = BigInteger.valueOf(0);
+        BigInteger b = BigInteger.valueOf(1);
+        for (int i = 0; i < n.intValue(); i++) {
+            BigInteger t = a;
+            a = b;
+            b = b.add(t);
+        }
+        return a;
+    }
+
+    private static BigInteger sum(BigInteger n) {
+        BigInteger one = BigInteger.valueOf(1);
+        BigInteger two = BigInteger.valueOf(2);
+        return fib(n.add(two)).subtract(one);
     }
 
     @Override
     public BigInteger perimeter(BigInteger n) {
-        return null;
+        BigInteger one = BigInteger.valueOf(1);
+        BigInteger four = BigInteger.valueOf(4);
+
+        return sum(n.add(one)).multiply(four);
     }
 
     @Override
     public double solveSum(double m) {
-        return 0;
+        return (1 - Math.sqrt(4 * m + 1)) / (2 * m) + 1;
+    }
+
+    private static ArrayList<Character> arrayToArrayList(char[] arr) {
+        ArrayList<Character> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+    private static String arrayListToString(ArrayList<Character> arrayList) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < arrayList.size(); i++) {
+            result.append(arrayList.get(i));
+        }
+        return result.toString();
     }
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        long min = n;
+        long f = 0, t = 0;
+        String num = String.valueOf(n);
+
+        for (int i = 0; i < num.length(); i++) {
+            for (int j = 0; j < num.length(); j++) {
+                char[] arr = num.toCharArray();
+                ArrayList<Character> arrayList = arrayToArrayList(arr);
+                char c = arr[i];
+                arrayList.remove(i);
+                arrayList.add(j, c);
+                long number = Long.parseLong(arrayListToString(arrayList));
+                if (number < min) {
+                    min = number;
+                    f = i;
+                    t = j;
+                }
+            }
+        }
+
+        return new long[] {min, f, t};
     }
 }
