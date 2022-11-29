de88690e8d881fd73d9e937ee55d1ee7b4dc94c2
Vladyslav Kovalov <vladkovalyov67@gmail.com>
2022-11-29 20:49:42 +0200

    commit

--- src/main/java/com/org/ita/kata/implementation/KovalyovVladyslav/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/KovalyovVladyslav/FiveImpl.java
@@ -8,31 +8,100 @@ import java.math.BigInteger;
 public class FiveImpl extends BaseKata implements Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+        int field = 0;
+
+        int numberOfField = v.length;
+        int[] leftSide = new int[numberOfField];
+        int[] rightSide = new int[numberOfField];
+
+        for (int i = 1; i < numberOfField; i++) {
+            if (v[i] >= v[i - 1]) {
+                leftSide[i] = leftSide[i - 1] + 1;
+            } else {
+                leftSide[i] = 0;
+            }
+        }
+
+        for (int i = v.length - 2; i >= 0; i--) {
+            if (v[i] >= v[i + 1]) {
+                rightSide[i] = rightSide[i + 1] + 1;
+            } else {
+                rightSide[i] = 0;
+            }
+        }
+        for (int i = 0; i < v.length; i++) {
+            field = Math.max(field, leftSide[i] + rightSide[i]);
+        }
+        return field + 1;
     }
 
+    private static boolean isPrime(long i) {
+        for (long j = 2; j < i / 2; j++) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
-    }
 
-    @Override
-    public int zeros(int n) {
-        return 0;
-    }
-
-    @Override
-    public BigInteger perimeter(BigInteger n) {
+        for (long i = m; i + g <= n; i++) {
+            if (isPrime(i) && isPrime(i + g) && gap(0, i + 1, i + g - 1) == null) {
+                return new long[]{i, i + g};
+            }
+        }
         return null;
     }
 
     @Override
+    public int zeros(int n) {
+        int count = 0;
+        while (n > 0) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+
+    @Override
+    public BigInteger perimeter(BigInteger n) {
+        BigInteger second = BigInteger.ONE, first = second, num = second.add(first), sum = second;
+        for(int i = 1; i <= n.intValue(); ++i) {
+            second = first;
+            first = num;
+            num = num.add(second);
+            sum = sum.add(second);
+        }
+        return sum.multiply(BigInteger.valueOf(4));
+    }
+
+    @Override
     public double solveSum(double m) {
-        return 0;
+        double s =  Math.sqrt(4 * m + 1);
+        return (1 - s) / (2 * m) + 1;
     }
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        long smol = n;
+        long delPos = 0;
+        long insPos = 0;
+        long tmpVal = 0;
+
+        StringBuilder sb = new StringBuilder().append(n);
+        int l = sb.length();
+
+        for (int i = 0; i < l; i++) for (int j = 0; j < l; j++) {
+            char c = sb.charAt(i);
+            sb.deleteCharAt(i).insert(j, c);
+            if ((tmpVal = Long.parseLong(sb.toString())) < smol) {
+                smol = tmpVal;
+                delPos = i;
+                insPos = j;
+            }
+            sb.deleteCharAt(j).insert(i, c);
+        }
+        return new long[] {smol, delPos, insPos};
     }
 }
--- src/main/java/com/org/ita/kata/implementation/KovalyovVladyslav/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/KovalyovVladyslav/SixImpl.java
@@ -3,6 +3,9 @@ package com.org.ita.kata.implementation.KovalyovVladyslav;
 import com.org.ita.kata.BaseKata;
 import com.org.ita.kata.Six;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SixImpl extends BaseKata implements Six {
     @Override
     public long findNb(long m) {
@@ -36,25 +39,125 @@ public class SixImpl extends BaseKata implements Six {
         double result = x / (1 + Math.sqrt(x + 1));
         return result;
     }
+    public static double[] arr(String town, String strng){
+        String num = null;
+        int town_s =strng.indexOf(town);
+        if (town_s == -1.0) {
+            return new double [0];
+        }
+        int town_e = strng.indexOf("\n",town_s);
+        if (town_e == -1){
+            num = strng.substring(town_s);
+        } else {
+            num = strng.substring(town_s,town_e);
+        }
+        String rejex = "\\d+\\.\\d";
+        double [] array = new double [12];
+        Pattern p = Pattern.compile(rejex);
+        Matcher m = p.matcher(num);
+        int i = 0;
+        while (m.find()){
+            array[i++] = Double.parseDouble(m.group());
+        }
+        return array;
+    }
 
     @Override
     public double mean(String town, String strng) {
-        return 0;
+        double [] array = arr(town,strng);
+        if (array.length == 0){
+            return -1;
+        }
+        double sum = 0.0;
+        for (double x : array){
+            sum+=x;
+        }
+        return (sum / 12);
     }
 
     @Override
     public double variance(String town, String strng) {
-
-        return 0;
+        double [] array = arr(town,strng);
+        if (array.length == 0){
+            return -1;
+        }
+        double mean = mean(town, strng);
+        double sum = 0;
+        for (double x : array){
+            sum+=Math.pow(x-mean,2);
+        }
+        return sum/12;
     }
 
     @Override
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+        if(toFind == "")
+            return "";
+        String res = toFind + ":";
+        int w = 0;
+        int d = 0;
+        int l = 0;
+        int s = 0;
+        int c = 0;
+        int p = 0;
+
+        if(!resultSheet.contains(toFind + " "))
+            return toFind + ":This team didn't play!";
+
+        String[] mas = resultSheet.split(",");
+        int score = 0;
+        int concede = 0;
+        for(int i = 0; i < mas.length; i++){
+            if(mas[i].contains(toFind)){
+                int j = mas[i].indexOf(toFind);
+                try {
+                    String[] splitStr = mas[i].split(" ");
+                    if(j == 0){
+                        score = Integer.parseInt(splitStr[toFind.split(" ").length]);
+                        concede = Integer.parseInt(splitStr[splitStr.length - 1]);
+                    }else{
+                        concede = Integer.parseInt(splitStr[splitStr.length - toFind.split(" ").length - 2]);
+                        score = Integer.parseInt(splitStr[splitStr.length - 1]);
+                    }
+                } catch (NumberFormatException e) {
+                    return "Error(float number):" + mas[i];
+                }
+                s += score;
+                c += concede;
+                if(score > concede){
+                    w++;
+                    p += 3;
+                }else if(score == concede){
+                    d++;
+                    p++;
+                }else{
+                    l++;
+                }
+            }
+        }
+        return res + "W=" + w + ";D=" + d + ";L=" + l
+                + ";Scored=" + s + ";Conceded=" + c
+                + ";Points=" + p;
     }
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        if (lstOfArt.length == 0 || lstOf1stLetter.length == 0)
+            return "";
+        String output="";
+
+        for (String letter : lstOf1stLetter){
+            int counter=0;
+            for (String stock : lstOfArt){
+                if (stock.charAt(0) == letter.charAt(0)){
+                    String[] parts = stock.split(" ");
+                    counter += Integer.parseInt(parts[1]);
+                }
+            }
+
+            output += "(" + letter.charAt(0) + " : " + counter + ") - ";
+        }
+        output = output.replaceAll(" - $", "");
+        return output;
     }
 }
