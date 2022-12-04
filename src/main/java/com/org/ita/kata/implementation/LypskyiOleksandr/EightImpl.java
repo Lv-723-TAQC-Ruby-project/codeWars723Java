f3f87650eb4246c24c58a3851c3ce4b00b11d27a
Edgers <a.b.23042000@gmail.com>
2022-12-04 20:38:26 +0200

    fix

--- src/main/java/com/org/ita/kata/implementation/LypskyiOleksandr/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/LypskyiOleksandr/EightImpl.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class EightImpl extends BaseKata implements Eight {
     @Override
